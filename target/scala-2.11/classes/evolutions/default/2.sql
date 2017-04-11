e0b6fa62d53452897d4ce6a1c048ad2229f2e2de
ShaneCotter <Shanecotter2472@gmail.com>
2017-04-11 15:14:57 +0100

    Formatted order confirmation & added out of stock message

--- target/scala-2.11/classes/evolutions/default/2.sql
+++ target/scala-2.11/classes/evolutions/default/2.sql
@@ -5,9 +5,15 @@ Insert into category(cat_id,name) values(3,'Comedy');
 Insert into category(cat_id,name) values(4,'Theatre');
 Insert into category(cat_id,name) values(5,'Racing');
 
-Insert into event (id,event_name,time,location,date,title,category_cat_id) values (1,'Ed Sheeran','8pm','3 Arena','27th June','Devide Tour',1);
-Insert into event (id,event_name,time,location,date,title,category_cat_id) values (2,'Coldplay','7pm','02 Arena','27th June','Tour',1);
-Insert into event (id,event_name,time,location,date,title,category_cat_id) values (3,'Motor Sport','2pm','Mondello Park','27th June','Racing Ireland',5);
+
+Insert into event (id,event_name,time,location,date,title,category_cat_id) values (1,'Ed Sheeran','20:00','3 Arena','14th April 2017','Divide Tour',1);
+Insert into event (id,event_name,time,location,date,title,category_cat_id) values (2,'Coldplay','19:00','Croke Park','8th July 2017','Head Full of Dreams Tour',1);
+Insert into event (id,event_name,time,location,date,title,category_cat_id) values (3,'European Grand Prix','12:00','Mondello Park','27th June 2017','Racing Ireland',5);
+Insert into event (id,event_name,time,location,date,title,category_cat_id) values (4,'Al Porter','20:00','Bord Gais Energy Theatre','27th November 2017','Al Porter Tour',3);
+Insert into event (id,event_name,time,location,date,title,category_cat_id) values (5,'The 1975','16:00','Malahide Castle','17th June 2017','The 1975 @ Malahide Castle',1);
+Insert into event (id,event_name,time,location,date,title,category_cat_id) values (6,'Bruno Mars','20:00','3 Arena','29th April 2017','24k Magic World Tour',1);
+Insert into event (id,event_name,time,location,date,title,category_cat_id) values (7,'BAMMA 30','17:00','3 Arena','7th July 2017','BAMMA Returns #GetReady',2);
+Insert into event (id,event_name,time,location,date,title,category_cat_id) values (8,'Liverpool vs. Sydney FC','17:00','ANZ Stadium','31st May 2017','End-of-Season Friendly',2);
 
 Insert into ticket values (1,'Standing',10,100,1);
 Insert into ticket values (2,'Seated',10,50,1);
