e504b7f637cf61427a869f7f29422b1f99e9a8d1
ShaneCotter <Shanecotter2472@gmail.com>
2017-04-18 14:57:08 +0100

    Implemented max quantity in cart

--- target/scala-2.11/twirl/main/views/html/cart.template.scala
+++ target/scala-2.11/twirl/main/views/html/cart.template.scala
@@ -92,45 +92,53 @@ Seq[Any](format.raw/*1.53*/("""
                                         <div class="col-xs-4 pull-right">
                                             <h4><strong>Quantity</strong></h4>
                                             """),_display_(/*64.46*/i/*64.47*/.getQuantity()),format.raw/*64.61*/("""
-                                        """),format.raw/*65.41*/("""<a href=""""),_display_(/*65.51*/routes/*65.57*/.ShoppingCtrl.removeOne(i.getId)),format.raw/*65.89*/(""""><span class="glyphicon glyphicon-minus-sign"></span></a>
-                                        <a href=""""),_display_(/*66.51*/routes/*66.57*/.ShoppingCtrl.addOne(i.getId)),format.raw/*66.86*/(""""><span class="glyphicon glyphicon-plus-sign"></span></a>
-                                        </div>
+                                            """),_display_(/*65.46*/if(i.getQuantity < i.getTicket.getQuantity)/*65.89*/ {_display_(Seq[Any](format.raw/*65.91*/("""
+                                                """),format.raw/*66.49*/("""<a href=""""),_display_(/*66.59*/routes/*66.65*/.ShoppingCtrl.removeOne(i.getId)),format.raw/*66.97*/(""""><span class="glyphicon glyphicon-minus-sign"></span></a>
+                                                <a href=""""),_display_(/*67.59*/routes/*67.65*/.ShoppingCtrl.addOne(i.getId)),format.raw/*67.94*/(""""><span class="glyphicon glyphicon-plus-sign"></span></a>
+                                            """)))}),format.raw/*68.46*/("""
+                                            """),_display_(/*69.46*/if(i.getQuantity == i.getTicket.getQuantity)/*69.90*/ {_display_(Seq[Any](format.raw/*69.92*/("""
+                                                """),format.raw/*70.49*/("""<a href=""""),_display_(/*70.59*/routes/*70.65*/.ShoppingCtrl.removeOne(i.getId)),format.raw/*70.97*/(""""><span class="glyphicon glyphicon-minus-sign"></span></a>
+                                                <span class="glyphicon glyphicon-plus-sign"></span>
+                                                <br>
+                                                Max reached
+                                            """)))}),format.raw/*74.46*/("""
+                                        """),format.raw/*75.41*/("""</div>
                                     </div>
                                 </div>
                                 <hr>
-                                """)))}),format.raw/*71.34*/("""
+                                """)))}),format.raw/*79.34*/("""
 
-                        """),format.raw/*73.25*/("""<div class="row">
+                        """),format.raw/*81.25*/("""<div class="row">
                             <div class="text-center">
                                 <div class="col-xs-9">
                                     <h6 class="text-right">Empty cart</h6>
                                 </div>
                                 <div class="col-xs-3">
-                                    <a href=""""),_display_(/*79.47*/routes/*79.53*/.ShoppingCtrl.emptyBasket()),format.raw/*79.80*/("""" class="btn btn-danger btn-sm" onclick="return confirmDel();">
+                                    <a href=""""),_display_(/*87.47*/routes/*87.53*/.ShoppingCtrl.emptyBasket()),format.raw/*87.80*/("""" class="btn btn-danger btn-sm" onclick="return confirmDel();">
                                         Empty Cart</a>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    """)))}),format.raw/*85.22*/("""
+                    """)))}),format.raw/*93.22*/("""
 
 
-                    """),format.raw/*88.21*/("""<div class="panel-footer">
+                    """),format.raw/*96.21*/("""<div class="panel-footer">
                         <div class="row text-center">
                             <div class="col-xs-9">
                                 <h4 class="text-right"><strong>
-                                    Total: &euro; """),_display_(/*92.52*/("%.2f".format(user.getBasket.getBasketTotal))),format.raw/*92.98*/("""</strong></h4>
+                                    Total: &euro; """),_display_(/*100.52*/("%.2f".format(user.getBasket.getBasketTotal))),format.raw/*100.98*/("""</strong></h4>
                             </div>
-                            """),_display_(/*94.30*/if(user.getBasket.getBasketTotal > 0)/*94.67*/ {_display_(Seq[Any](format.raw/*94.69*/("""
-                                """),format.raw/*95.33*/("""<div class="col-xs-3">
+                            """),_display_(/*102.30*/if(user.getBasket.getBasketTotal > 0)/*102.67*/ {_display_(Seq[Any](format.raw/*102.69*/("""
+                                """),format.raw/*103.33*/("""<div class="col-xs-3">
                                     <form action="/placeOrder" method="GET">
                                         <script
                                         src="https://checkout.stripe.com/checkout.js" class="stripe-button btn-success"
                                         data-key="pk_test_6pRNASCoBOKtIshFeQd4XMUh"
-                                        data-amount=""""),_display_(/*100.55*/user/*100.59*/.getBasket.getStripeTotal),format.raw/*100.84*/(""""
+                                        data-amount=""""),_display_(/*108.55*/user/*108.59*/.getBasket.getStripeTotal),format.raw/*108.84*/(""""
                                         data-name="TC Events"
                                         data-description="Payment"
-                                        data-email=""""),_display_(/*103.54*/user/*103.58*/.getEmail()),format.raw/*103.69*/(""""
+                                        data-email=""""),_display_(/*111.54*/user/*111.58*/.getEmail()),format.raw/*111.69*/(""""
                                         data-currency="EUR"
                                         data-image="http://cdn.wallpapersafari.com/62/83/v3qT9H.jpg"
                                         data-locale="auto"
@@ -138,8 +146,8 @@ Seq[Any](format.raw/*1.53*/("""
                                         </script>
                                     </form>
                                 </div>
-                            """)))}),format.raw/*111.30*/("""
-                        """),format.raw/*112.25*/("""</div>
+                            """)))}),format.raw/*119.30*/("""
+                        """),format.raw/*120.25*/("""</div>
                     </div>
                 </div>
             </div>
@@ -165,11 +173,11 @@ Seq[Any](format.raw/*1.53*/("""
 object cart extends cart_Scope0.cart
               /*
                   -- GENERATED --
-                  DATE: Tue Apr 18 13:50:31 BST 2017
+                  DATE: Tue Apr 18 14:49:15 BST 2017
                   SOURCE: C:/Users/Eileen/Desktop/TicketStore/app/views/cart.scala.html
-                  HASH: 5bddd8d5211f0434032f404b1a1292e11b70b36c
-                  MATRIX: 775->1|1020->52|1050->164|1082->170|1154->217|1180->235|1219->237|1254->245|2010->974|2025->980|2071->1005|2534->1441|2571->1469|2611->1471|2665->1497|2773->1574|2825->1599|2862->1627|2902->1629|2960->1659|3075->1747|3134->1790|3174->1792|3236->1826|3437->2000|3541->2094|3582->2096|3652->2138|3741->2199|3800->2236|3861->2279|3874->2284|3913->2285|3983->2327|4138->2451|4204->2489|4373->2631|4383->2632|4437->2664|4532->2732|4542->2733|4587->2757|4990->3133|5038->3160|5067->3161|5428->3495|5438->3496|5473->3510|5543->3552|5580->3562|5595->3568|5648->3600|5785->3710|5800->3716|5850->3745|6143->4007|6199->4035|6574->4383|6589->4389|6637->4416|6946->4694|7000->4720|7278->4971|7345->5017|7453->5098|7499->5135|7539->5137|7601->5171|8040->5582|8054->5586|8101->5611|8316->5798|8330->5802|8363->5813|8850->6268|8905->6294
-                  LINES: 27->1|35->1|37->7|38->8|39->9|39->9|39->9|41->11|54->24|54->24|54->24|62->32|62->32|62->32|63->33|65->35|67->37|67->37|67->37|68->38|69->39|69->39|69->39|72->42|75->45|75->45|75->45|76->46|76->46|76->46|77->47|77->47|77->47|78->48|79->49|80->50|82->52|82->52|82->52|83->53|83->53|83->53|89->59|89->59|89->59|94->64|94->64|94->64|95->65|95->65|95->65|95->65|96->66|96->66|96->66|101->71|103->73|109->79|109->79|109->79|115->85|118->88|122->92|122->92|124->94|124->94|124->94|125->95|130->100|130->100|130->100|133->103|133->103|133->103|141->111|142->112
+                  HASH: 50690af4ad6e08d02226e33f41d504c8999bcb03
+                  MATRIX: 775->1|1020->52|1050->164|1082->170|1154->217|1180->235|1219->237|1254->245|2010->974|2025->980|2071->1005|2534->1441|2571->1469|2611->1471|2665->1497|2773->1574|2825->1599|2862->1627|2902->1629|2960->1659|3075->1747|3134->1790|3174->1792|3236->1826|3437->2000|3541->2094|3582->2096|3652->2138|3741->2199|3800->2236|3861->2279|3874->2284|3913->2285|3983->2327|4138->2451|4204->2489|4373->2631|4383->2632|4437->2664|4532->2732|4542->2733|4587->2757|4990->3133|5038->3160|5067->3161|5428->3495|5438->3496|5473->3510|5547->3557|5599->3600|5639->3602|5717->3652|5754->3662|5769->3668|5822->3700|5967->3818|5982->3824|6032->3853|6167->3957|6241->4004|6294->4048|6334->4050|6412->4100|6449->4110|6464->4116|6517->4148|6869->4469|6939->4511|7133->4674|7189->4702|7564->5050|7579->5056|7627->5083|7936->5361|7990->5387|8269->5638|8337->5684|8446->5765|8493->5802|8534->5804|8597->5838|9036->6249|9050->6253|9097->6278|9312->6465|9326->6469|9359->6480|9846->6935|9901->6961
+                  LINES: 27->1|35->1|37->7|38->8|39->9|39->9|39->9|41->11|54->24|54->24|54->24|62->32|62->32|62->32|63->33|65->35|67->37|67->37|67->37|68->38|69->39|69->39|69->39|72->42|75->45|75->45|75->45|76->46|76->46|76->46|77->47|77->47|77->47|78->48|79->49|80->50|82->52|82->52|82->52|83->53|83->53|83->53|89->59|89->59|89->59|94->64|94->64|94->64|95->65|95->65|95->65|96->66|96->66|96->66|96->66|97->67|97->67|97->67|98->68|99->69|99->69|99->69|100->70|100->70|100->70|100->70|104->74|105->75|109->79|111->81|117->87|117->87|117->87|123->93|126->96|130->100|130->100|132->102|132->102|132->102|133->103|138->108|138->108|138->108|141->111|141->111|141->111|149->119|150->120
                   -- GENERATED --
               */
           
